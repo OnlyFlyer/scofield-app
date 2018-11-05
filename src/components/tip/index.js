9358e452d08973591448fc97819c616e66e5bd0b
wutao <m15776849661@163.com>
2018-11-06 00:28:03 +0800

    feat: Add export static resource feature by babel-plugin-module-resolver

--- src/components/tip/index.js
+++ src/components/tip/index.js
@@ -2,10 +2,10 @@
  * @Author: wutao
  * @Date: 2018-11-05 10:33:49
  * @Last Modified by: wutao
- * @Last Modified time: 2018-11-05 10:36:47
+ * @Last Modified time: 2018-11-06 00:24:41
  */
 
-import React, { ComponentState } from 'react'
+import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
 export default class Tip extends Component {
